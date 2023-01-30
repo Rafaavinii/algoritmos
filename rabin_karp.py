95ac199e7bb9723887b27dcbdb93c054bb70652f
Rafael Sousa <66565227+Rafaavinii@users.noreply.github.com>
2023-01-30 12:05:04 -0300

    Create rabin_karp.py

---
+++ rabin_karp.py
@@ -0,0 +1,30 @@
+def Rabin_Karp(T, n, P, m):
+    q = 3354393
+    d = 32
+    dM = 1
+
+    for i in range(m-1):
+        dM = (d*dM)%q
+    
+    h1 = 0
+
+    for i in range(m):
+        h1 = (h1*d + ord(P[i]))%q # mod q
+
+    h2 = 0
+    
+    for i in range(m):
+        h2 = (h2*d + ord(T[i]))%q # mod q
+    
+    i = 0
+
+    while h1 != h2 and i < n-m:
+
+        #if h1 == h2:
+        #   indices.append(i)
+
+        h2 = (h2+d*q - (ord(T[i])*dM))%q
+        h2 = (h2*d + ord(T[i+m]))%q
+        i = i + 1
+    
+    return i
